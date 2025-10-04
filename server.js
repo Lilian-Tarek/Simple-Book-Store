7e8855c96fb0ab102e4eb94f19c35a438bbc5ee4
Lilian <liliantarek4@gmail.com>
2025-10-04 23:11:22 +0300

    Fix vercel config for React + JSON server

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
-import jsonServer from "json-server";
-
+const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
-server.use(router);
+server.use("/api", router); // مهم يكون /api عشان يمشي مع routes في vercel.json
 
-export default server;
+module.exports = server;
