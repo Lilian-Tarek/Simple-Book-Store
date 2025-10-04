6cb140171b1f41140ca3c0dee283ae7772a93c46
Lilian <liliantarek4@gmail.com>
2025-10-04 23:29:38 +0300

    Fix white page and MIME type issue

--- server.js
+++ server.js
@@ -8,3 +8,10 @@ server.use(middlewares);
 server.use("/api", router);
 
 export default server;
+
+
+if (process.env.NODE_ENV !== "production") {
+  server.listen(9000, () => {
+    console.log("JSON Server is running on http://localhost:9000");
+  });
+}
